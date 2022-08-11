f376e104756d1ffa4802ba876634b720b0357159
PrBr20 <109619000+PrBr20@users.noreply.github.com>
2022-08-11 18:52:04 +0600

    connected cart_api and cartpage

--- router/indexRouter.js
+++ router/indexRouter.js
@@ -9,39 +9,42 @@ const cartRouter = require('./user/cart');
 
 const user_api = require('../database/user_api');
 const post_api = require('../database/post_api');
+const cart_api = require('../database/cart_api');
 
 router.get('/', async(req, res) =>{
-    res.render('userCartPage', {PageName : "cart"});
-    // if(!req.session.user_id){
-    //     console.log("Not logged in, redirecting to login page");
-    //     return res.redirect('/login');
-    // }
-    // console.log("homepage of user " + req.session.user_id);
+    //res.render('userCartPage', {PageName : "cart"});
+    if(!req.session.user_id){
+        console.log("Not logged in, redirecting to login page");
+        return res.redirect('/login');
+    }
+    console.log("homepage of user " + req.session.user_id);
 
-    // // For card_2
-    // let _topSellers = await user_api.getTopSellers(5);
-    // let _starSellers = await user_api.getStarSellers(5);
-    // let _newPosts = await post_api.getNewPosts(0, 5);
-    // let _mostReviewdPosts = await post_api.getMostReviewedPosts(0, 5);
+    // For card_2
+    let _topSellers = await user_api.getTopSellers(5);
+    let _starSellers = await user_api.getStarSellers(5);
+    let _newPosts = await post_api.getNewPosts(0, 5);
+    let _mostReviewdPosts = await post_api.getMostReviewedPosts(0, 5);
 
-    // // For card_1
-    // // TODO: Replace 1 with req.session.user_id
-    // let wishlistHomepagePosts = await post_api.getWishlistPosts(req.session.user_id, 0, 25);
-    // let newHomepagePosts = await post_api.getNewPosts(0, 25);
-    // let _homepagePosts = [...new Set([...wishlistHomepagePosts, ...newHomepagePosts])];
+    // For card_1
+    // TODO: Replace 1 with req.session.user_id
+    let wishlistHomepagePosts = await post_api.getWishlistPosts(req.session.user_id, 0, 25);
+    let newHomepagePosts = await post_api.getNewPosts(0, 25);
+    let _homepagePosts = [...new Set([...wishlistHomepagePosts, ...newHomepagePosts])];
+    let _cartPosts = await cart_api.getCartPosts(req.session.user_id);
 
-    // const binds = {
-    //     PageName: "home",
-    //     topSellers: _topSellers,
-    //     starSellers: _starSellers,
-    //     newPosts: _newPosts,
-    //     mostReviewdPosts: _mostReviewdPosts,
-    //     homePagePosts: _homepagePosts
-    //     // cart_item needs to be added
-    // };
-    // console.log(_homepagePosts[0].UPVOTECOUNT);
-    // res.render('userHomePage.ejs', binds);
-    // res.json(binds);
+    const binds = {
+        PageName: "home",
+        topSellers: _topSellers,
+        starSellers: _starSellers,
+        newPosts: _newPosts,
+        mostReviewdPosts: _mostReviewdPosts,
+        homePagePosts: _homepagePosts,
+        cartNum : _cartPosts.length
+        // cart_item needs to be added
+    };
+    console.log(_homepagePosts[0].UPVOTECOUNT);
+    res.render('userHomePage.ejs', binds);
+    //res.json(binds);
 });
 
 router.use('/login', loginRouter);
--- router/user/cart.js
+++ router/user/cart.js
@@ -7,16 +7,16 @@ const cart_api = require('../../database/cart_api');
 router.get('/', async(req, res) =>{
     let _cartPosts = await cart_api.getCartPosts(req.session.user_id);
     let _totalCartCostObj = await cart_api.getTotalCartCost(req.session.user_id);
-
     let _totalCartCost = 0;
     if(_totalCartCostObj[0].TOTALCOST != null)
         _totalCartCost = _totalCartCostObj[0].TOTALCOST;
 
     const binds = {
         cartPosts: _cartPosts,
-        totalCost: _totalCartCost
+        totalCost: _totalCartCost,
+        PageName: "cart"
     };
-    res.render('cartPage.ejs', binds);
+    res.render('userCartPage.ejs', binds);
 });
 
 router.post('/removepost', async(req, res) =>{
@@ -26,6 +26,7 @@ router.post('/removepost', async(req, res) =>{
 });
 
 router.post('/validate', async(req, res) =>{
+    console.log(req.body);
     await cart_api.validateCart(req.session.user_id, req.body.phone, req.body.address);
     res.redirect('/');
 });
@@ -33,6 +34,14 @@ router.post('/validate', async(req, res) =>{
 router.post('/addtocart', async(req, res) =>{
     let post_id = req.body.POST_ID;
     await cart_api.addToCart(req.session.user_id, post_id, 1);
+    let _cartPosts = await cart_api.getCartPosts(req.session.user_id);
+    res.send({cartNum : _cartPosts.length});
+});
+
+router.post('/updatecart', async(req, res) =>{
+    console.log(req.body);
+    await cart_api.updateQuantity(req.session.user_id, req.body.POST_ID, req.body.QUANTITY);
+    res.redirect('/');
 });
 
 module.exports = router;
