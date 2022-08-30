35786c958ecd6ece85b64f535d5596c081505753
afschy <1905081@ugrad.cse.buet.ac.bd>
2022-08-30 23:59:30 +0600

    created functioning search page

--- router/general/search.js
+++ router/general/search.js
@@ -2,7 +2,9 @@ const express = require('express');
 const path = require('path');
 const router = express.Router({mergeParams: true});
 
+const user_api = require('../../database/user_api');
 const post_api = require('../../database/post_api');
+const cart_api = require('../../database/cart_api');
 
 router.get('/', async(req, res) =>{
     let text = req.query.text.toString();
@@ -10,13 +12,25 @@ router.get('/', async(req, res) =>{
     let sortType = req.query.sortType.toString(); // ASC or DESC
 
     let result = await post_api.searchForPost(text, sortBy, sortType);
-    // console.log(result.productMatch);
-    // console.log(result.descriptionMatch);
-    // console.log(result.tagMatch);
+    let _topSellers = await user_api.getTopSellers(5);
+    let _starSellers = await user_api.getStarSellers(5);
+    let _newPosts = await post_api.getNewPosts(0, 5);
+    let _mostReviewdPosts = await post_api.getMostReviewedPosts(0, 5);
+
+    let _homepagePosts = [...new Set([...result.productMatch, ...result.descriptionMatch, ...result.tagMatch])];
+    let _cartPosts = await cart_api.getCartPosts(req.session.user_id);
+
     const binds = {
-        productMatch: result.productMatch,
-        descriptionMatch: result.descriptionMatch
+        PageName: "home",
+        topSellers: _topSellers,
+        starSellers: _starSellers,
+        newPosts: _newPosts,
+        mostReviewdPosts: _mostReviewdPosts,
+        homePagePosts: _homepagePosts,
+        cartNum : _cartPosts.length
+        // cart_item needs to be added
     };
+    res.render('userHomePage.ejs', binds);
 });
 
 module.exports = router;
